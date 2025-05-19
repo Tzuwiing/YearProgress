6d6fd569c44171a39c86e5f9c5d26a54d0a4db5e
Tzuwiing <jusuf1909@student.abudzar.sch.id>
2025-05-19 11:29:45 +0700

    w

---
+++ my-project/src/project/Index.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import StudentProfile from "./components/Profile";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <StudentProfile />
+    </div>
+  );
+}
+
+export default App;
